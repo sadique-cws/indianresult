45552b5c9fdac7a1aa4265ed43f51e230a9581db
sadique-cws <sadique.hussain96@gmail.com>
2020-12-09 08:14:49 +0530

    3rd day

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -3,9 +3,9 @@
 
 
 @section('content')
-    <div class="container">
+    <div class="container mt-4">
         <div class="row">
-            <div class="col-12">
+            <div class="col-6 mx-auto">
                 <form action="" method="POST">
                     @csrf
                     <div class="mb-3">
@@ -16,30 +16,36 @@
                         <label for="">contact</label>
                         <input type="text" name="contact" class="form-control">
                     </div>
-                    <div class="mb-3">
+                   <div class="row">
+                    <div class="mb-3 col">
                         <label for="">address</label>
                         <input type="text" name="address" class="form-control">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col">
                         <label for="">school</label>
                         <input type="text" name="school" class="form-control">
                     </div>
-                    <div class="mb-3">
-                        <label for="">maths</label>
-                        <input type="text" name="maths" class="form-control">
+                   </div>
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="">maths</label>
+                            <input type="text" name="maths" class="form-control">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="">sci</label>
+                            <input type="text" name="sci" class="form-control">
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="">sci</label>
-                        <input type="text" name="sci" class="form-control">
-                    </div>
-                    <div class="mb-3">
+                   <div class="row">
+                    <div class="mb-3 col">
                         <label for="">sst</label>
                         <input type="text" name="sst" class="form-control">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col">
                         <label for="">hindi</label>
                         <input type="text" name="hindi" class="form-control">
                     </div>
+                   </div>
                     <div class="mb-3">
                         <label for="">eng</label>
                         <input type="text" name="eng" class="form-control">
